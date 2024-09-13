fd9e89da126e410f4d35aa8682d8ba18a29ab31f
Rodney Ho <rodneyrhkk@gmail.com>
2024-09-13 00:40:26 -0700

    Update corsOptions.ts

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,6 +1,7 @@
 const corsOptions = {
 	origin: ['http://localhost:5173'],
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	allowedHeaders: 'Content-Type,Authorization',
 };
 
 export default corsOptions;
