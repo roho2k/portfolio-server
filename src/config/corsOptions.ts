ead6a2785ac8da0eaba8bb30ebb852c46f099e4c
Rodney Ho <rodneyrhkk@gmail.com>
2024-09-15 15:35:40 -0700

    add local/production environment variables

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,5 +1,12 @@
+import { NodeEnvironment } from '../utils/enums';
+
+const origin =
+	process.env.NODE_ENV === NodeEnvironment.Development
+		? 'http://localhost:5173'
+		: 'https://www.rodneyho.com';
+
 const corsOptions = {
-	origin: ['https://www.rodneyho.com'],
+	origin,
 };
 
 export default corsOptions;
